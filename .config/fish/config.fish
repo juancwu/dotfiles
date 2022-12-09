1a7db7a6d7f75b03fe205151a267fc57c6703c7e
Juan Wu <46619361+juancwu@users.noreply.github.com>
2022-12-09 16:08:56 -0500

    update(ubuntu): config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,8 +3,6 @@
 status is-interactive && eval /Users/jc/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
 # Go
 set -g GOPATH $HOME/go
 
