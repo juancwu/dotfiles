a55447436ca97cf4500921d98188577068a0a422
Juan Wu <46619361+juancwu@users.noreply.github.com>
2022-12-12 22:05:43 -0500

    remove(ubuntu): some unnecessary files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,17 @@
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-status is-interactive && eval /Users/jc/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+status is-interactive && eval /home/jc/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
 # Go
 set -g GOPATH $HOME/go
+set -g GOBIN $GOPATH/bin
+
+# Add gobin to path
+fish_add_path $GOBIN
+
+#x-server
+set -g DISPLAY $(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
 
 if type -q exa
   alias ll "exa -l -g --icons"
