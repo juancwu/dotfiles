f7c456fbd175e28c0a845dacfe135b88cdc66c63
juancwu <46619361+juancwu@users.noreply.github.com>
2022-12-15 22:54:41 -0500

    update: make config.fish to act as base config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
+set fish_greeting ""
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 status is-interactive && eval /home/jc/miniconda3/bin/conda "shell.fish" "hook" $argv | source
@@ -9,10 +11,21 @@ set -g GOPATH $HOME/go
 # Add gobin to path
 fish_add_path $GOPATH/bin
 
-#x-server
-set -g DISPLAY $(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
+# load configuration files based on OS
+switch (uname)
+  case Darwin
+    source (dirname (status --current-file))/config-darwin.fish
+  case Linux
+    source (dirname (status --current-file))/config-linux.fish
+end
 
 if type -q exa
   alias ll "exa -l -g --icons"
   alias lla "ll -a"
+end
+
+# allow local config overwrite
+set LOCAL_CONFIG (dirname (status --current-file))/config-local.fish
+if test -f $LOCAL_CONFIG
+  source $LOCAL_CONFIG
 end
