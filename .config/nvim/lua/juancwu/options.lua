fd316c0ede4d018afa83b140da3b24ba28c227fb
juancwu <46619361+juancwu@users.noreply.github.com>
2023-06-28 19:47:28 -0400

    some changes that i dont remember doing

--- .config/nvim/lua/juancwu/options.lua
+++ .config/nvim/lua/juancwu/options.lua
@@ -7,9 +7,9 @@ vim.opt.relativenumber = true -- juicy relativity
 
 vim.opt.autoindent = true
 vim.opt.smartindent = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smarttab = true
 vim.opt.breakindent = true
