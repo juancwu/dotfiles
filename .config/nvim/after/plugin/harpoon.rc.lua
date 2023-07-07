cf28bd8597c95be55cd68422351bc027352edf5f
juancwu <46619361+juancwu@users.noreply.github.com>
2023-07-07 01:11:50 -0400

    in progress: improving nvim workflow without system wide keybinds

---
+++ .config/nvim/after/plugin/harpoon.rc.lua
@@ -0,0 +1,18 @@
+local status, mark = pcall(require, "harpoon.mark")
+
+if not status then return end
+
+local ui
+status, ui = pcall(require, "harpoon.ui")
+
+if not status then return end
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<leader>q", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>w", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>e", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>r", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>,", ui.nav_prev)
+vim.keymap.set("n", "<leader>.", ui.nav_next)
--- .config/nvim/after/plugin/lspzero.rc.lua
+++ .config/nvim/after/plugin/lspzero.rc.lua
@@ -6,7 +6,40 @@ lspzero.preset("recommended")
 
 -- first argument is the client
 lspzero.on_attach(function(_, bufnr)
-    lspzero.default_keymaps({buffer = bufnr})
+    lspzero.default_keymaps({ buffer = bufnr })
+
+    -- custom keymaps
+    vim.keymap.set("n", "<leader>f", "<cmd>lua vim.lsp.buf.format()<CR>") -- format buffer
 end)
 
+-- Enable format on save
+lspzero.format_on_save({
+    format_opts = {
+        async = true,
+        timeout_ms = 10000,
+    },
+    servers = {
+        ['lua_ls'] = { 'lua' },
+    }
+})
+
+local lspconfig
+status, lspconfig = pcall(require, "lspconfig")
+
+if status then
+    lspconfig.lua_ls.setup(lspzero.nvim_lua_ls())
+end
+
 lspzero.setup()
+
+local cmp
+status, cmp = pcall(require, 'cmp')
+
+if not status then return end
+
+cmp.setup({
+    mapping = {
+        -- Press "Enter" to confirm completion/suggestion
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }
+})
