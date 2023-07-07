20f6897ab49f9f1a9ddf32334d05aa9d964a889f
juancwu <46619361+juancwu@users.noreply.github.com>
2023-07-07 01:11:50 -0400

    in progress: improving nvim workflow without system wide keybinds

--- .config/nvim/lua/juancwu/keymaps.lua
+++ .config/nvim/lua/juancwu/keymaps.lua
@@ -1,7 +1,12 @@
 vim.g.mapleader = " "
 
+-- easy escape
+vim.keymap.set("i", "<C-[>", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-[>", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-[>", "<Esc>", { noremap = true, silent = true })
+
 -- open the explorer
-vim.keymap.set("n", "<leader>e", "<cmd>Ex<CR>") 
+vim.keymap.set("n", "<leader>e", "<cmd>Ex<CR>")
 
 -- move highlighted lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -21,8 +26,10 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- do not copy with x, for god sake, WHY copy something that is being deleted??
 vim.keymap.set("n", "x", "\"_x")
 
--- smile
+-- smile :)
 vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "q", "<nop>", { noremap = true })
+vim.keymap.set("n", "<leader>q", "q", { noremap = true })
 
 -- select and replace
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
@@ -32,8 +39,8 @@ vim.keymap.set("n", "dd", "\"_dd")
 vim.keymap.set("n", "<leader>dd", "dd") -- cut line, under my control
 
 -- copy/paste to/from system clipboard
-vim.keymap.set({"n", "v"}, "<leader>y", "\"+y")
-vim.keymap.set({"n", "v"}, "<leader>p", "\"+p")
+vim.keymap.set({ "n", "v" }, "<leader>y", "\"+y")
+vim.keymap.set({ "n", "v" }, "<leader>p", "\"+p")
 
 -- increment/decrement a count, helpful for changing indeces
 vim.keymap.set("n", "+", "<C-a>")
@@ -51,7 +58,7 @@ vim.keymap.set("n", "<leader>db", "vbd")
 vim.keymap.set("n", "<C-a>", "gg<S-v>G") -- select all
 
 -- split pane
-vim.keymap.set("n", "ss", ":split<Return><C-w>w", { silent = true }) -- horizontal
+vim.keymap.set("n", "ss", ":split<Return><C-w>w", { silent = true })  -- horizontal
 vim.keymap.set("n", "sv", ":vsplit<Return><C-w>w", { silent = true }) -- vertical
 
 -- pane movement
@@ -66,3 +73,17 @@ vim.keymap.set("n", "<C-w><left>", "<C-w><")
 vim.keymap.set("n", "<C-w><right>", "<C-w>>")
 vim.keymap.set("n", "<C-w><up>", "<C-w>+")
 vim.keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- my arrow keys babyyyy
+vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true })
+vim.keymap.set("i", "<C-j>", "<Down>", { noremap = true })
+vim.keymap.set("i", "<C-k>", "<Up>", { noremap = true })
+vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true })
+
+-- terminal keymaps
+vim.keymap.set("t", "<C-t>", "<C-\\><C-n>", { noremap = true })
+vim.keymap.set("t", "<C-x>", "<cmd>bd!<CR>", { noremap = true })
+vim.keymap.set("n", "<C-t>", "<cmd>term<CR>i", { noremap = true })
+
+-- lazygit on floaterm
+vim.keymap.set("n", "<leader>g", "<cmd>FloatermNew lazygit<CR>", { noremap = true })
--- .config/nvim/lua/juancwu/packer.lua
+++ .config/nvim/lua/juancwu/packer.lua
@@ -2,31 +2,67 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  -- Themes
-  use({ 'rose-pine/neovim', as = 'rose-pine' })
+    -- Themes
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
 
-  -- Sweet LSP
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    requires = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
-      {                                      -- Optional
-        'williamboman/mason.nvim',
-        run = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end,
-      },
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    -- Sweet LSP
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                            -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},     -- Required
-      {'hrsh7th/cmp-nvim-lsp'}, -- Required
-      {'L3MON4D3/LuaSnip'},     -- Required
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
+        }
     }
-  }
+
+    -- Telescope for the fine file navigation
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+
+    use {
+        "nvim-telescope/telescope-file-browser.nvim",
+        requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    }
+
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+
+    -- autopairs, complete brackets
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
+
+    -- auto closing tags
+    use "windwp/nvim-ts-autotag"
+
+    -- quick navigation between files, harpoon
+    use 'ThePrimeagen/harpoon'
+
+    -- shows a history to undo
+    use 'mbbill/undotree'
+
+    -- float window for terminal commands
+    use 'voldikss/vim-floaterm'
 end)
