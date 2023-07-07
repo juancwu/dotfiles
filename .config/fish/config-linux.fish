228489f197a76f776daabe34cd1aee77184edb68
juancwu <46619361+juancwu@users.noreply.github.com>
2023-07-07 01:12:36 -0400

    put back DISPLAY env for copy pasta

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -1,6 +1,6 @@
 #x-server
-#set -g DISPLAY $(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
-#
-#if test -x (command -v npiperelay.exe)
-#  source (dirname (status --current-file))/config-npiperelay.fish
-#end
+set -g DISPLAY $(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
+
+if test -x (command -v npiperelay.exe)
+  source (dirname (status --current-file))/config-npiperelay.fish
+end
