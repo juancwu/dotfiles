dba3e617f7241de9da39e4a63364c1e054b594ce
juancwu <46619361+juancwu@users.noreply.github.com>
2023-09-11 16:43:49 -0400

    nvim config updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,15 +21,15 @@ require("keymaps")
 require("options")
 
 if is_wsl == 1 then
-	require("clipboard-wsl")
+  require("clipboard-wsl")
 end
 
 if is_mac == 1 then
-	require("clipboard-macos")
+  require("clipboard-macos")
 end
 
-if is_unix == 1 then
-	require("clipboard-unix")
+if is_unix == 1 and is_mac ~= 1 then
+  require("clipboard-unix")
 end
 
 require("lazy").setup("plugins")
---
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,6 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  config = {
+    show_end_of_line = true,
+  },
+}
