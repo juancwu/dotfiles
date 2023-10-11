3fbefe870b5c73b550194e990e0db8814f6d07f3
jc <46619361+juancwu@users.noreply.github.com>
2023-10-11 15:39:18 -0400

    update markdow preview plugin

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,9 +1,10 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
-  keys = {
-    { "<leader>mp", "<cmd>MarkdownPreview<CR>", desc = "Toggle [M]arkdown [P]review" }
-  }
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    build = function()
+        vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+        { "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>", desc = "Toggle [M]arkdown [P]review" }
+    }
 }
