8cfd9c319d6c1d520b0826e04a41f07733d4f491
jc <46619361+juancwu@users.noreply.github.com>
2023-12-19 15:35:20 -0500

    add js/ts to ft to format

--- nvim/lua/juancwu/plugins/lsp/init.lua
+++ nvim/lua/juancwu/plugins/lsp/init.lua
@@ -32,6 +32,10 @@ return {
 				opts = {
 					formatters_by_ft = {
 						lua = { "stylua" },
+						javascript = { "prettierd" },
+						typescript = { "prettierd" },
+						javascriptreact = { "prettierd" },
+						typescriptreact = { "prettierd" },
 					},
 					format_on_save = {
 						timeout_ms = 500,
