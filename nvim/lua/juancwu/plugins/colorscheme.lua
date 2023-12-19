93d64d99af1269c047d9517d0244d06c7ca476f2
jc <46619361+juancwu@users.noreply.github.com>
2023-12-18 23:27:22 -0500

    add more colorschemes

--- nvim/lua/juancwu/plugins/colorscheme.lua
+++ nvim/lua/juancwu/plugins/colorscheme.lua
@@ -1,18 +1,74 @@
 return {
+    -- rose-pine
     {
         "rose-pine/nvim",
         name = "rose-pine",
         priority = 1000,
     },
+
+    -- onedark
     {
         'navarasu/onedark.nvim',
         priority = 1000,
     },
+
+    -- solarized-osaka
     {
         "craftzdog/solarized-osaka.nvim",
         priority = 1000,
         opts = {
             transparent = false,
         }
-    }
+    },
+
+    -- tokyonight
+    {
+        "folke/tokyonight.nvim",
+        lazy = true,
+        opts = { style = "moon" },
+    },
+
+    -- catppuccin
+    {
+        "catppuccin/nvim",
+        lazy = true,
+        name = "catppuccin",
+        opts = {
+            integrations = {
+                aerial = true,
+                alpha = true,
+                cmp = true,
+                dashboard = true,
+                flash = true,
+                gitsigns = true,
+                headlines = true,
+                illuminate = true,
+                indent_blankline = { enabled = true },
+                leap = true,
+                lsp_trouble = true,
+                mason = true,
+                markdown = true,
+                mini = true,
+                native_lsp = {
+                    enabled = true,
+                    underlines = {
+                        errors = { "undercurl" },
+                        hints = { "undercurl" },
+                        warnings = { "undercurl" },
+                        information = { "undercurl" },
+                    },
+                },
+                navic = { enabled = true, custom_bg = "lualine" },
+                neotest = true,
+                neotree = true,
+                noice = true,
+                notify = true,
+                semantic_tokens = true,
+                telescope = true,
+                treesitter = true,
+                treesitter_context = true,
+                which_key = true,
+            },
+        },
+    },
 }
