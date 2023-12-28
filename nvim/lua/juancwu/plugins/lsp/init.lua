512ab888d135768fb747d79f31580e905ce1a915
jc <46619361+juancwu@users.noreply.github.com>
2023-12-28 14:11:44 -0500

    add commands to enable/disable format on save

--- nvim/lua/juancwu/plugins/lsp/init.lua
+++ nvim/lua/juancwu/plugins/lsp/init.lua
@@ -37,10 +37,12 @@ return {
 						javascriptreact = { "prettierd" },
 						typescriptreact = { "prettierd" },
 					},
-					format_on_save = {
-						timeout_ms = 500,
-						lsp_fallback = true,
-					},
+					format_on_save = function(bufnr)
+						if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+							return
+						end
+						return { timeout_ms = 500, lsp_fallback = true }
+					end,
 				},
 			},
 		},
