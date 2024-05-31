49fa962b70097225b80e49ee5658447af6b1c368
jc <46619361+juancwu@users.noreply.github.com>
2024-05-30 20:20:42 -0400

    enable obsidian + remove workspace

--- nvim/lua/juancwu/plugins/obsidian.lua
+++ nvim/lua/juancwu/plugins/obsidian.lua
@@ -3,7 +3,7 @@ return {
 	version = "*", -- recommended, use latest release instead of latest commit
 	lazy = true,
 	ft = "markdown",
-	enabled = false,
+	enabled = true,
 	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
 	-- event = {
 	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
@@ -22,10 +22,6 @@ return {
 			{
 				name = "personal",
 				path = "~/Documents/Obsidian Vault",
-			},
-			{
-				name = "work",
-				path = "~/vaults/work",
 			},
 		},
 
