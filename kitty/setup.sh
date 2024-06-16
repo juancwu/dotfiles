9160af93ce2b6756fbdfea690141d016495675eb
jc <46619361+juancwu@users.noreply.github.com>
2024-06-15 21:07:44 -0400

    (kitty setup) add support to use asadesuka

--- kitty/setup.sh
+++ kitty/setup.sh
@@ -2,14 +2,23 @@
 
 kitten @ set-font-size --all 11.0
 
-CURRENT_HOUR=$(date +"%H")
-SEVEN_AM=7
-SEVEN_PM=19
-
 TERM_COLOR_MODE=dark
-if [ $CURRENT_HOUR -ge $SEVEN_AM ] && [ $CURRENT_HOUR -lt $SEVEN_PM ]; then
-    TERM_COLOR_MODE=light
+
+command -v asadesuka > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    IS_ASA=$(asadesuka)
+    if [ $IS_ASA == "true" ]; then
+        TERM_COLOR_MODE=light
+    fi
+else
+    CURRENT_HOUR=$(date +"%H")
+    SEVEN_AM=7
+    SEVEN_PM=19
+    if [ $CURRENT_HOUR -ge $SEVEN_AM ] && [ $CURRENT_HOUR -lt $SEVEN_PM ]; then
+        TERM_COLOR_MODE=light
+    fi
 fi
+
 if [ $TERM_COLOR_MODE == "light" ]; then
     kitten @ set-colors --all "$HOME/.config/kitty/light.conf"
 else
