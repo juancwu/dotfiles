3fedaa4653dae6439f3f2d8774d1fd5c6772b37d
jc <46619361+juancwu@users.noreply.github.com>
2024-07-29 14:59:13 -0400

    (nvim) use prettier to format js/ts/md
    prettierd is dead, unmaintained

--- nvim/lua/juancwu/plugins/lsp/init.lua
+++ nvim/lua/juancwu/plugins/lsp/init.lua
@@ -33,10 +33,11 @@ return {
 					require("conform").setup({
 						formatters_by_ft = {
 							lua = { "stylua" },
-							javascript = { "prettierd" },
-							typescript = { "prettierd" },
-							javascriptreact = { "prettierd" },
-							typescriptreact = { "prettierd" },
+							javascript = { "prettier" },
+							typescript = { "prettier" },
+							javascriptreact = { "prettier" },
+							typescriptreact = { "prettier" },
+                            markdown = { "prettier" },
 							go = { "gofumpt" },
 							python = { "autopep8" },
 							yaml = { "yamlfmt" },
