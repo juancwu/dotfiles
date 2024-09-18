08481e3a0f4a61855eef40c926ff513ebbecc423
jc <46619361+juancwu@users.noreply.github.com>
2024-09-18 01:52:28 -0400

    (nvim) change obsidian search keybind

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -2,6 +2,6 @@ local map = vim.keymap.set
 
 map("v", "<leader>ln", "<CMD>ObsidianLinkNew<CR>")
 map("n", "<C-n>", "<CMD>ObsidianNew<CR>")
-map("n", "<leader>/", "<CMD>ObsidianSearch<CR>")
+map("n", "<leader>of", "<CMD>ObsidianSearch<CR>")
 map("n", "<leader>ll", "<CMD>ObsidianLinks<CR>")
 map("n", "<C-]>", "<CMD>ObsidianFollowLink<CR>")
