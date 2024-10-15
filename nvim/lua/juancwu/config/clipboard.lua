20dd4e841b9afc15372f2aecc3ee397742dd6c7e
jc <46619361+juancwu@users.noreply.github.com>
2024-10-15 17:10:50 -0400

    (nvim) use xorg or wayland for clipboard

--- nvim/lua/juancwu/config/clipboard.lua
+++ nvim/lua/juancwu/config/clipboard.lua
@@ -1,18 +1,35 @@
 local Utils = require("juancwu.utils")
 
 if Utils.os.is_linux() then
-    vim.g.clipboard = {
-        name = "xclip",
-        copy = {
-            ['+'] = "xclip -sel clip -i -quiet",
-            ['*'] = "xclip -sel primary -i -quiet",
-        },
-        paste = {
-            ['+'] = "xclip -sel clip -o -quiet",
-            ['*'] = "xclip -sel primary -o -quiet",
-        },
-        cache_enabled = 1,
-    }
+    local wayland_display = os.getenv("WAYLAND_DISPLAY")
+    local clipboard_cmd = "xclip"
+    if wayland_display then
+        vim.g.clipboard = {
+            name = "wl-clipboard",
+            copy = {
+                ['+'] = "wl-copy",
+                ['*'] = "wl-copy",
+            },
+            paste = {
+                ['+'] = "wl-paste",
+                ['*'] = "wl-paste",
+            },
+            cache_enabled = 1,
+        }
+    else
+        vim.g.clipboard = {
+            name = "xclip",
+            copy = {
+                ['+'] = "xclip -sel clip -i -quiet",
+                ['*'] = "xclip -sel primary -i -quiet",
+            },
+            paste = {
+                ['+'] = "xclip -sel clip -o -quiet",
+                ['*'] = "xclip -sel primary -o -quiet",
+            },
+            cache_enabled = 1,
+        }
+    end
 elseif Utils.os.is_mac() then
     vim.opt.clipboard:append { "unnamedplus" }
 elseif Utils.os.is_wsl() then
