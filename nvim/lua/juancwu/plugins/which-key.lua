1da8b06ac4c197938af0c0c1137fc32897f8dfb0
jc <46619361+juancwu@users.noreply.github.com>
2025-01-14 19:00:57 -0500

    (nvim) setup text keys if no nerd fonts for which-key

--- nvim/lua/juancwu/plugins/which-key.lua
+++ nvim/lua/juancwu/plugins/which-key.lua
@@ -8,7 +8,42 @@ return {
 
             local wk = require("which-key")
 
-            wk.setup()
+            wk.setup({
+                delay = 0,
+                icons = {
+                    mappings = vim.g.have_nerd_font,
+                    keys = vim.g.have_nerd_font and {} or {
+                        Up = '<Up> ',
+                        Down = '<Down> ',
+                        Left = '<Left> ',
+                        Right = '<Right> ',
+                        C = '<C-…> ',
+                        M = '<M-…> ',
+                        D = '<D-…> ',
+                        S = '<S-…> ',
+                        CR = '<CR> ',
+                        Esc = '<Esc> ',
+                        ScrollWheelDown = '<ScrollWheelDown> ',
+                        ScrollWheelUp = '<ScrollWheelUp> ',
+                        NL = '<NL> ',
+                        BS = '<BS> ',
+                        Space = '<Space> ',
+                        Tab = '<Tab> ',
+                        F1 = '<F1>',
+                        F2 = '<F2>',
+                        F3 = '<F3>',
+                        F4 = '<F4>',
+                        F5 = '<F5>',
+                        F6 = '<F6>',
+                        F7 = '<F7>',
+                        F8 = '<F8>',
+                        F9 = '<F9>',
+                        F10 = '<F10>',
+                        F11 = '<F11>',
+                        F12 = '<F12>',
+                    }
+                }
+            })
         end,
     }
 }
