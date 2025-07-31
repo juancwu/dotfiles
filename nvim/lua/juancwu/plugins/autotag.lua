78115fb3e009f79e33d4721216abaf4f9bba85ed
juancwu <46619361+juancwu@users.noreply.github.com>
2025-07-30 22:52:24 -0400

    update bashrc.arch and nvim

--- nvim/lua/juancwu/plugins/autotag.lua
+++ nvim/lua/juancwu/plugins/autotag.lua
@@ -2,8 +2,23 @@ return {
     "windwp/nvim-ts-autotag",
     dependencies = { "nvim-treesitter/nvim-treesitter", build = ':TSUpdate' },
     ft = {
+        'html',
+        'javascript',
+        'typescript',
         'javascriptreact',
         'typescriptreact',
+        'svelte',
+        'vue',
+        'tsx',
+        'jsx',
+        'xml',
+        'php',
+        'markdown',
+        'astro',
+        'glimmer',
+        'handlebars',
+        'hbs',
+        'templ',
     },
     config = function()
         local autotag = require('nvim-ts-autotag')
